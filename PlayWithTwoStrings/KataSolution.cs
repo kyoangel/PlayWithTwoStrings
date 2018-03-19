42fca8c7a21e539a29edcbdaf6304aa6c51d1d48
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2018-03-19 23:06:06 +0800

    complete kata, but not refactor yet

---
+++ PlayWithTwoStrings/KataSolution.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+
+namespace PlayWithTwoStrings
+{
+    public class KataSolution
+    {
+        private string _outputString;
+
+        public string WorkOnStrings(string firstString, string secondString)
+        {
+            _outputString = "";
+            var firstCharArray = firstString.ToCharArray();
+            var secondCharArray = secondString.ToCharArray();
+            foreach (var singleChar in firstCharArray)
+            {
+                var swapCount = secondCharArray.Count(x => char.ToLower(x) == char.ToLower(singleChar)) % 2;
+                if (swapCount == 1)
+                {
+                    _outputString += SwapCase(singleChar);
+                }
+                else
+                {
+                    _outputString += singleChar.ToString();
+                }
+            }
+            foreach (var singleChar in secondCharArray)
+            {
+                var swapCount = firstCharArray.Count(x => char.ToLower(x) == char.ToLower(singleChar)) % 2;
+                if (swapCount == 1)
+                {
+                    _outputString += SwapCase(singleChar);
+                }
+                else
+                {
+                    _outputString += singleChar.ToString();
+                }
+            }
+            return _outputString;
+        }
+
+        private string SwapCase(char singleChar)
+        {
+            return char.IsUpper(singleChar)
+                ? char.ToLower(singleChar).ToString()
+                : char.ToUpper(singleChar).ToString();
+        }
+    }
+}
---
+++ PlayWithTwoStringsTests1/KataSolutionTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlayWithTwoStrings.Tests
+{
+    [TestClass()]
+    public class KataSolutionTests
+    {
+        private KataSolution _kataSolution = new KataSolution();
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestMethod()]
+        public void two_string_no_same_char_just_append()
+        {
+            var actual = _kataSolution.WorkOnStrings("abc", "def");
+            Assert.AreEqual("abcdef", actual);
+        }
+
+        [TestMethod()]
+        public void two_string_has_only_one_same_char_just_append()
+        {
+            var actual = _kataSolution.WorkOnStrings("abc", "cde");
+            Assert.AreEqual("abCCde", actual);
+        }
+
+        [TestMethod()]
+        public void abab_with_bababa_should_be_ABABbababa()
+        {
+            var actual = _kataSolution.WorkOnStrings("abab", "bababa");
+            Assert.AreEqual("ABABbababa", actual);
+        }
+
+        [TestMethod()]
+        public void ab_with_aba_should_be_aBABA()
+        {
+            var actual = _kataSolution.WorkOnStrings("ab", "aba");
+            Assert.AreEqual("aBABA", actual);
+        }
+
+        [TestMethod()]
+        public void abcdeFgtrzw_with_defgGgfhjkwqe_should_be_abcDeFGtrzWDEFGgGFhjkWqE()
+        {
+            var actual = _kataSolution.WorkOnStrings("abcdeFgtrzw", "defgGgfhjkwqe");
+            Assert.AreEqual("abcDeFGtrzWDEFGgGFhjkWqE", actual);
+        }
+
+        [TestMethod()]
+        public void very_complicate_case()
+        {
+            var actual = _kataSolution.WorkOnStrings("eYHPTIOyZgVygK", "yzQeeJzFTZZZNPSuAeweiIklUNQLRKDmUPnOkxOvGtOQUIaIhLvuJGKPoFjrSONivMeXCfkcpaxPUuqlMhEgoiCn");
+            Assert.AreEqual("eyHpTIOYzGvYGkYZQEEJZFtzzzNpSuAEwEIiKlUNQLRkDmUpnoKxoVGToQUiaiHLVuJGkpOFjrSoNIVMEXCfKcPaxpUuqlMHegOICn", actual);
+        }
+    }
+}
